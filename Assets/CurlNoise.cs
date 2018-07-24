d54114ece2ce862b38f52a278643b0a1b2f5fae4
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2018-07-25 00:55:05 +0900

    Added Curl Noise on c#.

---
+++ Assets/CurlNoise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurlNoiseSample : MonoBehaviour {
+    public static float EPSILON = 1e-3f;
+
+    public static Vector3 CurlNoise(Vector3 coord)
+    {
+        Vector3 dx = new Vector3(EPSILON, 0.0f, 0.0f);
+        Vector3 dy = new Vector3(0.0f, EPSILON, 0.0f);
+        Vector3 dz = new Vector3(0.0f, 0.0f, EPSILON);
+
+        Vector3 dpdx0 = new Vector3(Simplex.Noise.Generate(coord - dx), 0.0f, 0.0f);
+        Vector3 dpdx1 = new Vector3(Simplex.Noise.Generate(coord + dx), 0.0f, 0.0f);
+        Vector3 dpdy0 = new Vector3(0.0f, Simplex.Noise.Generate(coord - dy), 0.0f);
+        Vector3 dpdy1 = new Vector3(0.0f, Simplex.Noise.Generate(coord + dy), 0.0f);
+        Vector3 dpdz0 = new Vector3(0.0f, 0.0f, Simplex.Noise.Generate(coord - dz));
+        Vector3 dpdz1 = new Vector3(0.0f, 0.0f, Simplex.Noise.Generate(coord + dz));
+
+        float x = dpdy1.z - dpdy0.z + dpdz1.y - dpdz0.y;
+        float y = dpdz1.x - dpdz0.x + dpdx1.z - dpdx0.z;
+        float z = dpdx1.y - dpdx0.y + dpdy1.x - dpdy0.x;
+
+        return new Vector3(x, y, z) / EPSILON * 2.0f;
+    }
+}
---
+++ Assets/Example.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public List<OneParticle> ps = new List<OneParticle>();
+    public Vector3 additionalVector;
+    public float emitterSize = 10f;
+    public float convergence = 4f;
+    public float viscosity = 5f;
+    public Vector2 times;
+    private float idleTime = 3f;
+    public float timer = 0.0f;
+
+    public class OneParticle {
+        public Vector3 emitPos;
+        public Vector3 position;
+        public Vector4 velocity; //xyz = velocity, w = velocity coef
+        public Vector3 life;     // x = time elapsed, y = life time, z = isActive 1 is active, -1 is disactive
+        public Vector3 size;     // x = current size, y = start size, z = target size.
+        public Vector4 color;
+        public Vector4 startColor;
+        public Vector4 endColor;
+    };
+
+    // Use this for initialization
+    void Start () {
+        this.ps = new List<OneParticle>();
+        for (int i = 0; i < 1000; i++)
+        {
+            OneParticle particle = new OneParticle();
+            if (particle.life.z == -1) {
+                particle.life = new Vector3(0.0f, particle.life.y, 1.0f);
+                particle.position = particle.emitPos * Mathf.Clamp(times.y * 0.3f, 0.0f, 1.0f);
+                particle.size.x = particle.size.y;
+                particle.color = particle.startColor;
+            }
+            this.ps.Add(p);
+            GameObject anObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            anObj.transform.position;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.timer += Time.deltaTime;
+        if (this.timer <= this.idleTime) return; // just for idling
+
+        this.times = new Vector2(Time.deltaTime, this.timer);
+
+        for (int i = 0; i < this.ps.Count; i++) {
+            OneParticle particle = this.ps[i];
+            if (particle.life.z == 1.0) {
+                particle.life.x += times.x;
+                if (particle.life.x > particle.life.y) {
+                    particle.life.z = -1.0f;
+                } else {
+                    Vector3 velocityXYZ = (Vector3)particle.velocity; //Vector4 to Vector3
+                    Vector3 force = CurlNoiseSample.CurlNoise(
+                        particle.position * times.x * this.convergence
+                    ) - velocityXYZ;
+                    velocityXYZ += (force + additionalVector) * times.x *
+                            this.viscosity * Mathf.Clamp(times.y * 0.5f, 0.0f, 1.0f);
+                    particle.position += velocityXYZ * times.x;
+
+                    particle.size.x = Mathf.Lerp(
+                        particle.size.y,
+                        particle.size.z,
+                        Mathf.Clamp(particle.life.x / particle.life.y, 0.0f, 1.0f)
+                    ) * Mathf.Clamp(times.y * 0.1f, 0.0f, 1.0f);
+
+                    particle.color = Vector4.Lerp(
+                        particle.startColor, particle.endColor,
+                        Mathf.Clamp(particle.life.x / particle.life.y, 0.0f, 1.0f) *
+                        Mathf.Clamp(times.y, 0.0f, 1.0f)
+                    );
+                }
+            }
+            //代入作業
+            
+        }
+    }
+
+}
